d71b350435efca7b2816f99be30880215569d4c6
JARVIS <jarvisweng@hotmail.com>
2019-12-01 16:25:37 -0500

    [SOFT-35] added events

--- projects/pedal_board/src/brake_fsm.c
+++ projects/pedal_board/src/brake_fsm.c
@@ -8,12 +8,28 @@ FSM_DECLARE_STATE(pressed);
 FSM_DECLARE_STATE(released);
 
 FSM_STATE_TRANSITION(pressed) {
-	FSM_ADD_TRANSITION(
+	FSM_ADD_TRANSITION(RELEASED, released);
+};
 
-void brake_fsm_init() {
+FSM_STATE_TRANSITION(released) {
+	FSM_ADD_TRANSITION(PRESSED, released);
+}
+
+static void brake_fsm_init() {
+	fsm_state_init(pressed, );
+	fsm_state_init(released, );
+
+	fsm_init(&brake_fsm, "Brake_FSM", &released, &brake_fsm);
+}
+
+static void brake_fsm_process_event() {
 
 }
 
-void brake_fsm_process_event() {
+static void prv_pressed_output(Fsm *fsm, const Event *e) {
+	
+}
+
+static void prv_released_output(Fsm *fsm, const Event *e) {
 
 }
