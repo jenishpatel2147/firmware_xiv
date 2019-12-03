eac670581c37222c16222ba6b99461bfc86638ac
JARVIS <jarvisweng@hotmail.com>
2019-12-03 18:15:53 -0500

    fixed changes and added a main

--- projects/pedal_board/src/brake_fsm.c
+++ projects/pedal_board/src/brake_fsm.c
@@ -1,35 +1,59 @@
+#include "ads1015.h"
+#include "event_queue.h"
 #include "fsm.h"
-#include "events_quere.h"
 #include "log.h"
+#include "events.h"
 
-static Fsm brake_fsm;
+Fsm *brake_fsm;
 
 FSM_DECLARE_STATE(pressed);
 FSM_DECLARE_STATE(released);
 
 FSM_STATE_TRANSITION(pressed) {
-	FSM_ADD_TRANSITION(RELEASED, released);
+  FSM_ADD_TRANSITION(BRAKE_RELEASED, released);
 };
 
 FSM_STATE_TRANSITION(released) {
-	FSM_ADD_TRANSITION(PRESSED, released);
+  FSM_ADD_TRANSITION(BRAKE_PRESSED, pressed);
 }
 
-static void brake_fsm_init() {
-	fsm_state_init(pressed, );
-	fsm_state_init(released, );
+static void prv_callback_channel1(Ads1015Channel channel, void *context) {
+  Ads1015Storage *storage = context;
+  int16_t position = 0;
+  ads1015_read_raw(storage, channel, &position);
 
-	fsm_init(&brake_fsm, "Brake_FSM", &released, &brake_fsm);
+  // math to convert readings to angles
+  // position =
+
+  if (position > (3.14 / 4)) {
+    event_raise(BRAKE_PRESSED, 1);
+  } else {
+    event_raise(BRAKE_RELEASED, 0);
+  }
 }
 
-static void brake_fsm_process_event() {
-
+static void prv_pressed_output(Fsm *fsm, const Event *e, void *context) {
+  // raises a can event to main, which sends can message
+  event_raise(CAN_BRAKE_PRESSED, 1);
 }
 
-static void prv_pressed_output(Fsm *fsm, const Event *e) {
-	
+static void prv_released_output(Fsm *fsm, const Event *e, void *context) {
+  // raises a can event to main, which sends can message
+  event_raise(CAN_BRAKE_RELEASED, 0);
 }
 
-static void prv_released_output(Fsm *fsm, const Event *e) {
+bool brake_fsm_process_event(Event *e) {
+  // transition the state
+  return fsm_process_event(brake_fsm, e);
+}
 
+//main should have a brake fsm, and ads1015storage
+void brake_fsm_init(Fsm *brake, Ads1015Storage *storage) {
+  fsm_state_init(pressed, prv_pressed_output);
+  fsm_state_init(released, prv_released_output);
+  brake_fsm = brake;
+
+  fsm_init(brake, "Brake_FSM", &released, brake);
+
+  ads1015_configure_channel(storage, storage->current_channel, true, prv_callback_channel1, storage);
 }
--- projects/pedal_board/src/main.c
+++ projects/pedal_board/src/main.c
@@ -1,21 +1,61 @@
+#include "ads1015.h"
 #include "can.h"
+#include "event_queue.h"
+#include "fsm.h"
 #include "gpio.h"
+#include "gpio_it.h"
 #include "interrupt.h"
-#include "event_quere.h"
-//include all the modules
-//
+#include "log.h"
+// include all the modules
+#include "brake_fsm.h"
+#include "events.h"
 
+
+#define CAN_DEVICE_ID 0x1
+
+static Fsm brake_fsm;
+static Ads1015Storage ads1015_storage;
+static CanStorage can_storage;
+static Ads1015Storage ads_storage;
 
 int main() {
-	//initialize all the modules
-	gpio_init();
-	interrupt_init();
-	event_quere_init();
+  // initialize all the modules
+  gpio_init();
+  interrupt_init();
+  gpio_it_init();
+  soft_timer_init();
+  event_queue_init();
 
-	while(true) {
-		
-	}
+  const CanSettings can_settings = {
+    .device_id = CAN_DEVICE_ID,
+    .bitrate = CAN_HW_BITRATE_500KBPS,
+    .rx_event = CAN_RX,
+    .tx_event = CAN_TX,
+    .fault_event = CAN_FAULT,
+    .tx = { GPIO_PORT_A, 12 },  // CHANGE
+    .rx = { GPIO_PORT_A, 11 },  // CHANGE
+  };
+  can_init(&can_storage, &can_settings);
+  brake_fsm_init(&brake_fsm, &ads1015_storage);
 
+  // setup ADC readings
+  I2CSettings i2c_settings = {
+    .speed = I2C_SPEED_FAST,  //
+    .scl = { .port = GPIO_PORT_B, .pin = 5 },	//figure out later
+    .sda = { .port = GPIO_PORT_B, .pin = 5 },  //figure out later
+  };
+  status_ok_or_return(i2c_init(I2C_PORT_2, &i2c_settings));
+  GpioAddress ready_pin = { .port = GPIO_PORT_B, .pin = 5 };	//CHANGE
+  status_ok_or_return(ads1015_init(&ads1015_storage, I2C_PORT_2, ADS1015_ADDRESS_GND, &ready_pin));
 
-	return 0;
+  Event e = { 0 };
+  while (true) {
+    event_process(&e);
+	brake_fsm_process_event(&e);
+	LOG_DEBUG("working");
+	//perhaps distinguis which events are actually for can
+    can_process_event(&e);	
+  }
+
+  return 0;
 }
