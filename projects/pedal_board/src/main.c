4e531d8c01a95421813a50f36247cf35dde686ef
JARVIS <jarvisweng@hotmail.com>
2019-12-05 19:52:26 -0500

    Added test files

--- projects/pedal_board/src/main.c
+++ projects/pedal_board/src/main.c
@@ -1,3 +1,4 @@
+#include "ads1015.h"
 #include "can.h"
 #include "event_queue.h"
 #include "fsm.h"
@@ -5,20 +6,21 @@
 #include "gpio_it.h"
 #include "interrupt.h"
 #include "log.h"
-#include "ads1015.h"
 // include all the modules
 #include "brake_fsm.h"
 #include "events.h"
-
+#include "test_helpers.h"
+#include "unity.h"
+#include "status.h"
 
 #define CAN_DEVICE_ID 0x1
 
 static Fsm brake_fsm;
 static Ads1015Storage ads1015_storage;
 static CanStorage can_storage;
-static Ads1015Storage ads_storage;
 
 int main() {
+  LOG_DEBUG("WORKING\n");
   // initialize all the modules
   gpio_init();
   interrupt_init();
@@ -43,20 +45,21 @@ int main() {
     .scl = { .port = GPIO_PORT_B, .pin = 5 },  // figure out later
     .sda = { .port = GPIO_PORT_B, .pin = 5 },  // figure out later
   };
-  status_ok_or_return(i2c_init(I2C_PORT_2, &i2c_settings));
+  i2c_init(I2C_PORT_2, &i2c_settings);
   GpioAddress ready_pin = { .port = GPIO_PORT_B, .pin = 5 };  // CHANGE
-  status_ok_or_return(ads1015_init(&ads1015_storage, I2C_PORT_2, ADS1015_ADDRESS_GND, &ready_pin));
+  ads1015_init(&ads1015_storage, I2C_PORT_2, ADS1015_ADDRESS_GND, &ready_pin);
 
   brake_fsm_init(&brake_fsm, &ads1015_storage);
 
-  Event e = { 0 };
-  while (true) {
-    event_process(&e);
-    brake_fsm_process_event(&e);
-    LOG_DEBUG("working");
-    // perhaps distinguis which events are actually for can
-    can_process_event(&e);
-  }
-
+/*
+    Event e = { 0 };
+    while (true) {
+      event_process(&e);
+      brake_fsm_process_event(&e);
+      LOG_DEBUG("working\n");
+      // perhaps distinguis which events are actually for can
+      can_process_event(&e);
+    }
+    */
   return 0;
 }
